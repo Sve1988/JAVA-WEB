c997f9c67d67c4c7770eef7f47785a29bd23b7fd
Sve1988 <68242735+Sve1988@users.noreply.github.com>
2022-06-05 11:05:17 +0300

    Add files via upload

---
+++ DemoMVC/src/test/java/bg/softuni/DemoMVC/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.DemoMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
